a370fe5127b8c568559561977e218c816c7fbbe0
raheez66 <raheez66@gmail.com>
2023-02-18 15:53:21 +0400

    loading the time from api completed

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:logger/logger.dart';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -13,33 +13,10 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    String url = 'https://jsonplaceholder.typicode.com/posts/1';
-    final response = await get(Uri.parse(url));
-    print(response.body);
-    Map data = jsonDecode(response.body);
-    String name = data['title'];
-    String age = data['body'];
-    Logger().d('value_of_name_'+name);
-    Logger().d('value_of_body_'+age);
-  }
-
   void getTime() async {
-    String url = 'https://www.worldtimeapi.org/api/timezone/Europe/London';
-    final response = await get(Uri.parse(url));
-    print(response.body);
-    Map data = jsonDecode(response.body);
-    String dateTime = data['datetime'];
-    String utc_offset = data['utc_offset'].toString().substring(1,3);
-    Logger().d('value_of_dateTime_'+dateTime);
-    Logger().d('value_of_utc_offset_'+utc_offset);
-    
-    DateTime now = DateTime.parse(dateTime);
-    Logger().d('value_of_now_'+now.toString());
-
-    now = now.add(Duration(hours: int.parse(utc_offset)));
-    Logger().d('value_of_now_'+now.toString());
-
+    WorldTime worldTime = WorldTime(location: 'berlin', flag: 'berlin.png', url: 'europe');
+    await worldTime.getTime();
+    print('fetched_value_is_'+worldTime.time);
   }
 
   @override
@@ -47,6 +24,7 @@ class _LoadingState extends State<Loading> {
     super.initState();
     getTime();
   }
+
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/services/world_time.dart
@@ -0,0 +1,37 @@
+
+import 'package:http/http.dart';
+import 'package:logger/logger.dart';
+import 'dart:convert';
+
+class WorldTime{
+
+  String location;
+  String flag;
+  String time='';
+  String url;
+
+  WorldTime({required this.location,required this.flag,required this.url});
+
+
+  Future<void> getTime() async {
+    String url = 'https://www.worldtimeapi.org/api/timezone/Europe/London';
+    final response = await get(Uri.parse(url));
+    print(response.body);
+    Map data = jsonDecode(response.body);
+    String dateTime = data['datetime'];
+    String utc_offset = data['utc_offset'].toString().substring(1,3);
+    Logger().d('value_of_dateTime_'+dateTime);
+    Logger().d('value_of_utc_offset_'+utc_offset);
+
+    DateTime now = DateTime.parse(dateTime);
+    Logger().d('value_of_now_'+now.toString());
+
+    now = now.add(Duration(hours: int.parse(utc_offset)));
+    Logger().d('value_of_now_'+now.toString());
+
+    time = now.toString();
+
+  }
+}
+
+WorldTime instance = WorldTime(location: 'berlin', flag: 'germany.png' , url: 'europe/berlin');
