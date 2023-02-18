c0f22815dbbde6ef58366f7947c1a57e69006f86
raheez66 <raheez66@gmail.com>
2023-02-18 12:39:19 +0400

    worked with async and await

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
-
   @override
   State<ChooseLocation> createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-
   int counter = 0;
+
+  void getData() async {
+    //simulate nw request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      print('yoshi');
+      return 'yoshi';
+    });
+
+    String bio = await Future.delayed(Duration(seconds: 4), () {
+      print('this is the bio');
+      return 'this is the bio';
+    });
+
+    print('statement');
+    print('$username-$bio');
+  }
 
   @override
   void initState() {
     super.initState();
     print('init_state_run');
+    getData();
   }
 
   @override
@@ -26,11 +41,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body:ElevatedButton.icon(onPressed: (){
-        setState(() {
-          counter +=1;
-        });
-      }, icon: Icon(Icons.ac_unit), label: Text('counter value is ${counter}')),
+      body: ElevatedButton.icon(
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+          icon: Icon(Icons.ac_unit),
+          label: Text('counter value is ${counter}')),
     );
   }
 }
