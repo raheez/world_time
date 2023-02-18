7113ac12f7806c9f5503b70321d652c659a5051d
raheez66 <raheez66@gmail.com>
2023-02-18 14:43:19 +0400

    integrated world time api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -17,14 +17,35 @@ class _LoadingState extends State<Loading> {
     String url = 'https://jsonplaceholder.typicode.com/posts/1';
     final response = await get(Uri.parse(url));
     print(response.body);
-    var data = json.decode(response.body);
-    Logger().d(data);
+    Map data = jsonDecode(response.body);
+    String name = data['title'];
+    String age = data['body'];
+    Logger().d('value_of_name_'+name);
+    Logger().d('value_of_body_'+age);
+  }
+
+  void getTime() async {
+    String url = 'https://www.worldtimeapi.org/api/timezone/Europe/London';
+    final response = await get(Uri.parse(url));
+    print(response.body);
+    Map data = jsonDecode(response.body);
+    String dateTime = data['datetime'];
+    String utc_offset = data['utc_offset'].toString().substring(1,3);
+    Logger().d('value_of_dateTime_'+dateTime);
+    Logger().d('value_of_utc_offset_'+utc_offset);
+    
+    DateTime now = DateTime.parse(dateTime);
+    Logger().d('value_of_now_'+now.toString());
+
+    now = now.add(Duration(hours: int.parse(utc_offset)));
+    Logger().d('value_of_now_'+now.toString());
+
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
